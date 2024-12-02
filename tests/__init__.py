449baac7f1233853edf02ba28b4db8ea2b2ac48b
Matt Nicholson <nicholson.matt94@gmail.com>
2024-12-02 14:02:22 -0500

    add test cases, all passing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,4 @@
-#
+
+from pathlib import Path
+
+INPUT_DIR = Path(__file__).parents[1].joinpath('input')
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,26 +1,38 @@
+"""
+Tests for Day 1 solutions.
 
+Usage
+-----
+> cd AdventOfCode2024
+> python -m unittest tests.test_day1
+"""
 import unittest
+
+from .__init__ import INPUT_DIR
+from day1 import calc_distance, parse_input, quicksort
 
 
 class TestDay1(unittest.TestCase):
     """Test cases for Day 1."""
 
-    input = [
-        '3   4',
-        '4   3'
-        '2   5',
-        '1   3',
-        '3   9',
-        '3   3',
-    ]
+    def setUp(self) -> None:
+        """Set up before every test case."""
+        self.input_path = INPUT_DIR.joinpath('day1_test.txt')
+        self.list1, self.list2 = parse_input(self.input_path)
 
     def test_parse_input(self) -> None:
         """Test parse_input() function."""
-        pass
+        self.assertEqual(self.list1, [3, 4, 2, 1, 3, 3])
+        self.assertEqual(self.list2, [4, 3, 5, 3, 9, 3])
+
+    def test_quicksort(self) -> None:
+        """Test custom quicksort()."""
+        self.assertEqual(quicksort(self.list1), [1, 2, 3, 3, 3, 4])
+        self.assertEqual(quicksort(self.list2), [3, 3, 3, 4, 5, 9])
 
     def test_sample_input(self) -> None:
-        """Test sample input."""
-        pass
+        """Test calc_distance() with sample input."""
+        self.assertEqual(calc_distance(self.list1, self.list2), 11)
 
 
 if __name__ == '__main__':
