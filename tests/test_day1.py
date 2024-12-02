4d8c8373cd0cda26e689cbcd6eae7214fb1e45f9
Matt Nicholson <nicholson.matt94@gmail.com>
2024-12-02 13:08:07 -0500

    Add files via upload

---
+++ tests/test_day1.py
@@ -0,0 +1,27 @@
+
+import unittest
+
+
+class TestDay1(unittest.TestCase):
+    """Test cases for Day 1."""
+
+    input = [
+        '3   4',
+        '4   3'
+        '2   5',
+        '1   3',
+        '3   9',
+        '3   3',
+    ]
+
+    def test_parse_input(self) -> None:
+        """Test parse_input() function."""
+        pass
+
+    def test_sample_input(self) -> None:
+        """Test sample input."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
