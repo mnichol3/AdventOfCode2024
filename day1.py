ec4b720427ef55e42348858fdc067c3ac21a4a9e
Matt Nicholson <nicholson.matt94@gmail.com>
2024-12-02 14:01:41 -0500

    implement solution

--- day1.py
+++ day1.py
@@ -3,14 +3,78 @@
 from pathlib import Path
 
 
-StarList = list[list[int, int]]
+def calc_distance(list1: list[int], list2: list[int]) -> int:
+    """Calculate the distances between sorted values in the input list.
+
+    Parameters
+    ----------
+    list1: list of int
+    list2: list of int
+
+    Returns
+    -------
+    int
+    """
+    sum = 0
+    list1 = quicksort(list1)
+    list2 = quicksort(list2)
+
+    if len(list1) != len(list2):
+        len_str = f'{len(list1)}, {len(list2)}'
+        raise ValueError(f'Input lists must have same length, got {len_str}')
+
+    for idx, val1 in enumerate(list1):
+        val2 = list2[idx]
+
+        if val1 >= val2:
+            minuend = val1
+            subtrahend = val2
+        else:
+            minuend = val2
+            subtrahend = val1
+
+        sum += minuend - subtrahend
+
+    return sum
 
 
-def calc_distance(input: StarList) -> int:
-    pass
+def quicksort(arr: list[int]) -> list[int]:
+    """Generic quicksort algorithm.
+
+    Slower than built-in Python sorting algorithms but wheres the fun in using
+    those?
+
+    Parameters
+    ----------
+    arr: list of int
+        List of integers to sort.
+
+    Returns
+    -------
+    list of int
+        Sorted list of integers.
+    """
+    lesser = []
+    equal = []
+    greater = []
+
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+
+    for x in arr:
+        if x < pivot:
+            lesser.append(x)
+        elif x == pivot:
+            equal.append(x)
+        elif x > pivot:
+            greater.append(x)
+
+    return quicksort(lesser) + equal + quicksort(greater)
 
 
-def parse_input(f_path: Path) -> StarList:
+def parse_input(f_path: Path) -> list[list[int]]:
     """Read the input from a file and return it as a nest list of ints.
 
     Parameters
@@ -25,4 +89,12 @@ def parse_input(f_path: Path) -> StarList:
     if not isinstance(f_path, Path):
         f_path = Path(f_path)
 
-    return list(zip(*[x.split() for x in f_path.read_text().split('\n') if x]))
+    a, b = list(zip(*[x.split() for x in f_path.read_text().split('\n') if x]))
+
+    return [int(x) for x in a], [int(y) for y in b]
+
+
+if __name__ == '__main__':
+    input = parse_input(Path.cwd().joinpath('input', 'day1.txt'))
+    dist = calc_distance(*input)
+    print(dist)
