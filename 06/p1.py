dc655ee2e2aaeb3e135574df70aa4a0c680323be
mnichol3 <nicholson.matt94@gmail.com>
2024-12-08 17:12:23 -0500

    refactor

--- 06/p1.py
+++ 06/p1.py
@@ -1,4 +1,5 @@
 """Advent of Code 2024 - Day 6 Part 1"""
+from itertools import cycle
 from pathlib import Path
 
 
@@ -45,53 +46,45 @@ def part1(guard_map: list[str]) -> int:
     5. Get unique map coordinates.
     """
 
-    def get_starting_pos(guard_map: list[str]) -> tuple[tuple[int, int], str]:
+    def get_starting_pos(guard_map: list[str]) -> tuple[int, int]:
         for i in range(len(guard_map)):
             for j in range(len(guard_map[0])):
-                if guard_map[i][j] in movement.keys():
-                    return (i, j), guard_map[i][j]
+                if guard_map[i][j] == '^':
+                    return i, j
 
-        return (-1, -1), ''
+        return -1, -1
 
-    next_dir = {
-        '^': '>',
-        '>': 'v',
-        'v': '<',
-        '<': '^',
-    }
+    movement = cycle([
+        (-1, 0),
+        (0, 1),
+        (1, 0),
+        (0, -1),
+    ])
 
-    movement = {
-        '^': (-1, 0),
-        '>': (0, 1),
-        'v': (1, 0),
-        '<': (0, -1),
-    }
-
-    visited = []
     ni = len(guard_map)
     nj = len(guard_map[0])
 
-    (curr_i, curr_j), curr_dir = get_starting_pos(guard_map)
+    curr_i, curr_j = get_starting_pos(guard_map)
 
     if (curr_i, curr_j) == (-1, -1):
         return 0
 
+    visited = set([(curr_i, curr_j)])
+    move = next(movement)
     while True:
-        visited.append((curr_i, curr_j))
-
-        next_step = movement[curr_dir]
-        next_i = curr_i + next_step[0]
-        next_j = curr_j + next_step[1]
+        next_i = curr_i + move[0]
+        next_j = curr_j + move[1]
 
         if not 0 <= next_i < ni or not 0 <= next_j < nj:
-            return len(list(set(visited)))
+            return len(visited)
 
         if guard_map[next_i][next_j] == '#':
-            curr_dir = next_dir[curr_dir]
+            move = next(movement)
         else:
             curr_i = next_i
             curr_j = next_j
+            visited.add((curr_i, curr_j))
 
 
 if __name__ == '__main__':
-    print(f'Day 06 Part 1 answer: {part1(parse_input("input_test.txt"))}')
+    print(f'Day 06 Part 1 answer: {part1(parse_input("input.txt"))}')
