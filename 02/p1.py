71eb3c7fcbf807475380ffa6b7db9936d3321bfe
mnichol3 <nicholson.matt94@gmail.com>
2024-12-07 19:42:44 -0500

    make it not suck

--- 02/p1.py
+++ 02/p1.py
@@ -2,40 +2,27 @@
 from pathlib import Path
 
 
-INPUT_PATH = Path(__file__).parent.joinpath('input', 'day2.txt')
+INPUT_PATH = Path(__file__).parents[1].joinpath('input', 'day2.txt')
 
 
-def valid_report(report: list[int]) -> bool:
+def valid_report(report: list[int], valid_seq: list[int]) -> bool:
     """Validate a report.
 
     Parameters
     ----------
     report: list of int
+    valid_seq: list of int
 
     Returns
     -------
     bool
         True if report is valid, else False.
     """
-    valid_len = len(report) - 1
-    num_pos = 0
-    num_in_range = 0
+    for i in range(len(report) - 1):
+        if report[i+1] - report[i] not in valid_seq:
+            return False
 
-    for i in range(valid_len):
-        diff = report[i+1] - report[i]
-        if diff > 0:
-            num_pos += 1
-
-        if 1 <= abs(diff) <= 3:
-            num_in_range += 1
-
-    if (
-        num_in_range == valid_len and
-        (num_pos == 0 or num_pos == valid_len)
-    ):
-        return True
-
-    return False
+    return True
 
 
 def parse_input(f_path: Path) -> list[list[int]]:
@@ -56,10 +43,13 @@ def parse_input(f_path: Path) -> list[list[int]]:
     ]
 
 
-def get_solution(reports: list[list[int]]) -> int:
-    """Get the number of valid reports."""
-    return len([y for y in (valid_report(x) for x in reports) if y])
-
-
 if __name__ == '__main__':
-    print(f'Day 2 Part 1 solution: {get_solution(parse_input(INPUT_PATH))}')
+    num_valid = 0
+    inc_seq = range(1, 4)
+    dec_seq = range(-3, 0)
+
+    for report in parse_input(INPUT_PATH):
+        if valid_report(report, inc_seq) or valid_report(report, dec_seq):
+            num_valid += 1
+
+    print(f'Day 02 Part 1 solution: {num_valid}')
