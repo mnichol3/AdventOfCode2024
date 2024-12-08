99e8c1ff20add8f0c75635a059feacecf5c9e72b
mnichol3 <nicholson.matt94@gmail.com>
2024-12-07 21:56:18 -0500

    ugly but works:

---
+++ 04/p1.py
@@ -0,0 +1,79 @@
+"""Advent of Code 2024 - Day 4 Part 1"""
+from pathlib import Path
+
+
+def parse_input() -> list[str]:
+    """Parse the input from a text file.
+
+    Parameters
+    ----------
+    None.
+
+    Returns
+    -------
+    list of list of str
+    """
+    f_path = Path(__file__).parents[1].joinpath('input', 'day4.txt')
+    return [x for x in f_path.read_text().split('\n') if x]
+
+
+def count_matches(puzzle, m, n):
+    num = 0
+    if puzzle[m][n] != 'X':
+        return 0
+
+    row_len = len(puzzle[m])
+    pzl_len = len(puzzle)
+
+    if n + 3 < row_len and ''.join(puzzle[m][n+i] for i in range(4)) == 'XMAS':
+        # Forward along row
+        num += 1
+    if n - 3 >= 0 and ''.join(puzzle[m][n-i] for i in range(4)) == 'XMAS':
+        # Backward along row
+        num += 1
+    if m + 3 < pzl_len and ''.join(puzzle[m+i][n] for i in range(4)) == 'XMAS':
+        # Downward along column
+        num += 1
+    if m - 3 >= 0 and ''.join(puzzle[m-i][n] for i in range(4)) == 'XMAS':
+        # Upward along column
+        num += 1
+    if (
+        m + 3 < pzl_len and n + 3 < row_len and
+        ''.join(puzzle[m+i][n+i] for i in range(4)) == 'XMAS'
+    ):
+        # Downward along column, forward along row
+        num += 1
+    if (
+        m + 3 < pzl_len and n - 3 >= 0 and
+        ''.join(puzzle[m+i][n-i] for i in range(4)) == 'XMAS'
+    ):
+        # Downward along column, backward along row
+        num += 1
+    if (
+        m - 3 >= 0 and n + 3 < row_len and
+        ''.join(puzzle[m-i][n+i] for i in range(4)) == 'XMAS'
+    ):
+        # Upward along column, forward along row
+        num += 1
+    if (
+        m - 3 >= 0 and n - 3 >= 0 and
+        ''.join(puzzle[m-i][n-i] for i in range(4)) == 'XMAS'
+    ):
+        # Upward along column, backward along row
+        num += 1
+
+    return num
+
+
+def answer(puzzle: list[str]) -> any:
+    n = 0
+
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            n += count_matches(puzzle, i, j)
+
+    return n
+
+
+if __name__ == '__main__':
+    print(f'Day 04 Part 1 answer: {answer(parse_input())}')
