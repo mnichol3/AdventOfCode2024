d7fd7e48892a562c00f62a715e110d27e1fca1f3
mnichol3 <nicholson.matt94@gmail.com>
2024-12-08 20:37:36 -0500

    correct solution but incredibly slow lol

---
+++ 07/p2.py
@@ -0,0 +1,57 @@
+"""Advent of Code 2024 - Day 7 Part 2"""
+import re
+from itertools import product
+from operator import add, mul
+
+from p1 import parse_input
+
+
+def concat(a: int, b: int) -> int:
+    """Concatenate two integers."""
+    return int(f'{a}{b}')
+
+
+def part2(calibrations: list[str]) -> int:
+    """Solution to Part 1.
+
+    Parameters
+    ----------
+    calibrations: list of str
+
+    Returns
+    -------
+    int
+        Calibration results.
+
+    Problem
+    -------
+    * Each line is a single equation.
+    * Numbers can either be multiplied or added to equal the test value.
+    * Operators are always evaluated left to right.
+    * We now have the concetenation operator.
+
+    Walkthrough
+    -----------
+    * Same approach as in Part 1.
+    """
+    valid_sum = 0
+    for line in calibrations:
+        nums = [int(x) for x in re.findall(r'\d+', line)]
+
+        test_val = nums[0]
+        operands = nums[1:]
+
+        for combo in product([add, mul, concat], repeat=len(operands)-1):
+            total = operands[0]
+            for i in range(len(combo)):
+                total = combo[i](total, operands[i+1])
+
+            if total == test_val:
+                valid_sum += test_val
+                break
+
+    return valid_sum
+
+
+if __name__ == '__main__':
+    print(f'Day 07 Part 1 answer: {part2(parse_input())}')
