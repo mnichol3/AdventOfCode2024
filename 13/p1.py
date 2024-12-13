606d35a103f2cc14f1f53028eeaeea33919c076b
mnichol3 <nicholson.matt94@gmail.com>
2024-12-13 02:37:51 -0500

    working solution for p1 & p2

---
+++ 13/p1.py
@@ -0,0 +1,90 @@
+"""Advent of Code 2024 - Day 13 Part 1"""
+from __future__ import annotations
+import re
+from dataclasses import dataclass
+from pathlib import Path
+
+
+class Button:
+
+    def __init__(self, name, x: int, y: int, cost: int):
+        self.name = name
+        self.x = int(x)
+        self.y = int(y)
+        self.cost = int(cost)
+
+    def __repr__(self) -> str:
+        sub_str = f'{self.name} X+{self.x}, Y+{self.y}'
+        return f'<{self.__class__.__qualname__} {sub_str}>'
+
+
+@dataclass
+class ButtonContainer:
+    A: Button
+    B: Button
+
+
+class ClawMachine:
+
+    def __init__(
+        self,
+        button_a: tuple[int, int],
+        button_b: tuple[int, int],
+        prize: tuple[int, int],
+    ):
+        self.buttons = ButtonContainer(
+            Button('A', *button_a, 3),
+            Button('B', *button_b, 1),
+        )
+        self.prize = prize
+
+    def solve(self, part2: bool = False) -> tuple[int, int]:
+
+        def _is_int(x: int) -> bool:
+            return round(x, 2).is_integer()
+
+        prize_x = self.prize[0]
+        prize_y = self.prize[1]
+
+        if part2:
+            prize_x += 10000000000000
+            prize_y += 10000000000000
+
+        c = prize_x / self.buttons.A.x
+        d = self.buttons.B.x / self.buttons.A.x
+        b = ((prize_y - (self.buttons.A.y*c))
+             / (self.buttons.B.y - (self.buttons.A.y*d)))
+
+        a = c - d * b
+
+        if _is_int(a) and _is_int(b) and a > 0 and b > 0:
+            return (a*self.buttons.A.cost) + (b*self.buttons.B.cost)
+
+        return 0
+
+    @classmethod
+    def from_file(cls, f_path: str = 'input.txt') -> list[ClawMachine]:
+        machines = []
+        for block in Path(f_path).read_text().split('\n\n'):
+            inp = re.findall(r'X(?:\+|\=)(\d+), Y(?:\+|\=)(\d+)', block)
+            machines.append(
+                cls(
+                    tuple(map(int, inp[0])),
+                    tuple(map(int, inp[1])),
+                    tuple(map(int, inp[2])),
+                ))
+
+        return machines
+
+    def __repr__(self) -> str:
+        prize = f'<Prize X={self.prize[0]}, Y={self.prize[1]}'
+        sub_str = f'{self.buttons.A}, {self.buttons.B}, {prize}>'
+        return f'<{self.__class__.__qualname__} {sub_str}>'
+
+
+if __name__ == '__main__':
+    sum = 0
+    for m in ClawMachine.from_file():
+        sum += m.solve(True)
+
+    print(f'Answer: {int(sum)}')
--- README.md
+++ README.md
@@ -10,5 +10,5 @@ My solutions for [Advent of Code 2024](https://adventofcode.com/2024/about)
 | Day 3 :star::star: | Day 10 :star::star: | Day 17              | Day 24 :santa:      | :beers::clock10::confetti_ball::beers::clock10::confetti_ball: |
 | Day 4 :star::star: | Day 11 :star::star: | Day 18              | Day 25 :christmas_tree::gift: | :snowflake::snowman::snowflake::snowman::snowflake::snowman::snowflake: |
 | Day 5 :star::star: | Day 12 :star::star: | Day 19              | :snowflake::snowman::snowflake::snowman::snowflake::snowman::snowflake: | :snowman::snowflake::snowman::snowflake::snowman::snowflake::snowman: |
-| Day 6 :star:       | Day 13              | Day 20              | :snowman::snowflake::snowman::snowflake::snowman::snowflake::snowman: | :snowflake::snowman::snowflake::snowman::snowflake::snowman::snowflake: |
+| Day 6 :star:       | Day 13 :star::star: | Day 20              | :snowman::snowflake::snowman::snowflake::snowman::snowflake::snowman: | :snowflake::snowman::snowflake::snowman::snowflake::snowman::snowflake: |
 | Day 7 :star::star: | Day 14              | Day 21              | :snowflake::snowman::snowflake::snowman::snowflake::snowman::snowflake: | :snowman::snowflake::snowman::snowflake::snowman::snowflake::snowman: |
